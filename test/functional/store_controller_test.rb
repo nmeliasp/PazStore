4d581d9d6c54ae2ec653e91a8fb9d63a1ae2c1d3
Elias <nmeliasp@gmail.com>
2011-11-07 15:56:06 -0700

    Catalog Display and Cart Addition

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -4,6 +4,10 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '#columns #side a', :minimum => 1
+    assert_select '#main .entry', 2
+    assert_select 'h3', 'Blackout'
+    assert_select '.price', /\$[,\d]+\.\d\d/
   end
 
 end
